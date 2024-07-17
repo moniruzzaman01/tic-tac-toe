51924e56ae08ec4fb3ec1807097ddaa87f2fa9f7
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-07-17 22:48:26 +0600

    player functionality added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,40 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Box from "./Box";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isPlayerX, setIsPlayerX] = useState(true);
   console.log(squares);
 
   const handleClick = (index) => {
     console.log("clicked", index);
+    if (squares[index]) return;
     const tempSquare = [...squares];
-    tempSquare[index] = "X";
+    if (isPlayerX) tempSquare[index] = "X";
+    else tempSquare[index] = "O";
+    setIsPlayerX(!isPlayerX);
     setSquares([...tempSquare]);
   };
 
   return (
-    <div className=" border-2 rounded border-slate-400 p-5 flex flex-col gap-5 ">
-      <div className=" grid grid-cols-3 gap-2">
-        {squares.map((element, index) => (
-          <Box
-            key={index}
-            value={element}
-            handleClick={() => handleClick(index)}
-          />
-        ))}
+    <div>
+      <h3 className=" text-white mb-2 uppercase flex gap-1">
+        Player{" "}
+        <span className=" text-orange-600 text-2xl block text-center leading-6 font-extrabold h-6 w-6">
+          {isPlayerX ? "X" : "O"}
+        </span>{" "}
+        turn
+      </h3>
+      <div className=" border-2 rounded border-slate-400 p-5 flex flex-col gap-5 ">
+        <div className=" grid grid-cols-3 gap-2">
+          {squares.map((element, index) => (
+            <Box
+              key={index}
+              value={element}
+              handleClick={() => handleClick(index)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
