f649daf5ea5558b014a880a7e4e1a3f507ca2085
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-07-17 23:20:43 +0600

    winner calculate function added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,14 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Box from "./Box";
+import { checkWinner } from "../utils/checkWinner";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isPlayerX, setIsPlayerX] = useState(true);
-  console.log(squares);
+  //   console.log(squares);
+  const winner = checkWinner(squares);
 
   const handleClick = (index) => {
-    console.log("clicked", index);
     if (squares[index]) return;
     const tempSquare = [...squares];
     if (isPlayerX) tempSquare[index] = "X";
@@ -18,11 +19,11 @@ export default function Board() {
 
   return (
     <div>
-      <h3 className=" text-white mb-2 uppercase flex gap-1">
-        Player{" "}
+      <h3 className=" text-white mb-2 uppercase flex gap-1 justify-center">
+        Player
         <span className=" text-orange-600 text-2xl block text-center leading-6 font-extrabold h-6 w-6">
           {isPlayerX ? "X" : "O"}
-        </span>{" "}
+        </span>
         turn
       </h3>
       <div className=" border-2 rounded border-slate-400 p-5 flex flex-col gap-5 ">
---
+++ src/utils/checkWinner.js
@@ -0,0 +1,22 @@
+const probableResult = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+export const checkWinner = (squares) => {
+  for (let i = 0; i < probableResult.length; i++) {
+    const [a, b, c] = probableResult[i];
+
+    if (squares[a] && squares[a] == squares[b] && squares[a] == squares[c]) {
+      console.log("true");
+      return true;
+    }
+  }
+  return null;
+};
